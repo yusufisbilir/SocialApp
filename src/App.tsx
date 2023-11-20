a9283e7a217a3d81aa0b5e8ecdf348d03a93b864
yusufisbilir <dev.yusufisbilir@gmail.com>
2023-11-20 21:39:11 +0300

    folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
-export const App = () => {
-  return <div>Social App</div>;
+import { Routes, Route } from 'react-router-dom';
+import { ROUTES } from './_constants/ROUTES';
+import SignIn from './_auth/forms/SignIn';
+import SignUp from './_auth/forms/SignUp';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
+
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+      <Routes>
+        {/* public */}
+        <Route path='/' element={<AuthLayout />}>
+          <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+          <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+        </Route>
+        {/* private */}
+        <Route path='/' element={<RootLayout />}>
+          <Route index path={ROUTES.HOME} element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
+
+export default App;
---
+++ src/_constants/ROUTES.ts
@@ -0,0 +1,5 @@
+export const ROUTES = {
+  HOME: '/',
+  SIGN_UP: '/signup',
+  SIGN_IN: '/sign-in',
+};
