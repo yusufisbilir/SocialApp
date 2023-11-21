1c4a8480a14c1ee0ef6e9ddb75628ae8b2ddaf13
yusufisbilir <dev.yusufisbilir@gmail.com>
2023-11-21 21:25:43 +0300

    add login side image

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,25 @@
+import { Outlet, Navigate } from 'react-router-dom';
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuthenticated = false;
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to='/' />
+      ) : (
+        <>
+          <section className='flex flex-col items-center justify-center py-10 grow'>
+            <Outlet />
+          </section>
+
+          <img
+            src='/assets/images/login-side-image.jpg'
+            alt='login'
+            className='hidden object-cover w-1/2 h-screen bg-no-repeat xl:block'
+          />
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
