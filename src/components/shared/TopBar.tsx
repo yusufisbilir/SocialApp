00e7972c57c8d233b5fc0b9d680eb61476591afb
yusufisbilir <dev.yusufisbilir@gmail.com>
2023-11-27 20:53:49 +0300

    complete sign out

--- src/components/shared/TopBar.tsx
+++ src/components/shared/TopBar.tsx
@@ -1,8 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../ui/button';
 import { IconLogout } from '@tabler/icons-react';
+import { useSignOut } from '@/lib/react-query/queriesMutations';
+import { useEffect } from 'react';
+import { ROUTES } from '@/_constants/ROUTES';
 
 const TopBar = () => {
+  const { mutate: signOut, isSuccess } = useSignOut();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(ROUTES.SIGN_IN);
+    }
+  }, [isSuccess]);
+
   return (
     <section className='sticky top-0 z-50 w-full md:hidden bg-dark-2'>
       <div className='flex items-center justify-between p-4'>
@@ -10,7 +22,7 @@ const TopBar = () => {
           <h1 className='text-lg font-semibold'>@SocialApp</h1>
         </Link>
         <div className='flex gap-4'>
-          <Button variant='ghost'>
+          <Button variant='ghost' onClick={() => signOut()}>
             <IconLogout />
           </Button>
         </div>
