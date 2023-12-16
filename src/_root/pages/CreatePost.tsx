ca2867bf2f54d9d991f17edb9963c156c0a5010b
yusufisbilir <dev.yusufisbilir@gmail.com>
2023-12-16 22:40:51 +0300

    Create post title

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,5 +1,16 @@
+import { ImagePlus } from 'lucide-react';
+
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <div className='flex flex-1'>
+      <div className='flex flex-col items-center flex-1 gap-10 px-5 py-10 overflow-auto md:px-8 lg:p-14'>
+        <div className='flex items-center w-full max-w-5xl gap-3'>
+          <ImagePlus size={36} />
+          <h2 className='text-xl font-semibold'>Create Post</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
