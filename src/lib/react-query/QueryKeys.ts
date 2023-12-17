4caaf7295dcc01a47542bb3ceee1204ecdcf857f
yusufisbilir <dev.yusufisbilir@gmail.com>
2023-12-17 23:19:05 +0300

    add useCreatePost mutation

---
+++ src/lib/react-query/QueryKeys.ts
@@ -0,0 +1,20 @@
+export enum QUERY_KEYS {
+  // AUTH
+  CREATE_USER_ACCOUNT = 'createUserAccount',
+
+  // USER
+  GET_CURRENT_USER = 'getCurrentUser',
+  GET_USERS = 'getUsers',
+  GET_USER_BY_ID = 'getUserById',
+
+  // POST
+  GET_POSTS = 'getPosts',
+  GET_INFINITE_POSTS = 'getInfinitePosts',
+  GET_RECENT_POSTS = 'getRecentPosts',
+  GET_POST_BY_ID = 'getPostById',
+  GET_USER_POSTS = 'getUserPosts',
+  GET_FILE_PREVIEW = 'getFilePreview',
+
+  //  SEARCH
+  SEARCH_POSTS = 'getSearchPosts',
+}
--- src/lib/react-query/queriesMutations.ts
+++ src/lib/react-query/queriesMutations.ts
@@ -1,6 +1,8 @@
 import { INewUser, ISignIn } from '@/types/user';
-import { useMutation } from '@tanstack/react-query';
-import { createUser, signIn, signOutUser } from '../appwrite/api';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { createPost, createUser, signIn, signOutUser } from '../appwrite/api';
+import { INewPost } from '@/types/post';
+import { QUERY_KEYS } from './QueryKeys';
 
 export const useCreateUser = () => {
   return useMutation({
@@ -17,5 +19,17 @@ export const useSignIn = () => {
 export const useSignOut = () => {
   return useMutation({
     mutationFn: () => signOutUser(),
+  });
+};
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
   });
 };
